7c24d7bde7334e50b7ed4bcded1c98ea2679cb8d
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-01-26 17:56:05 +0200

    6_commit

--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("bookshop2", String(process.env.DB_USERNAME), process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(String(process.env.DB_NAME), String(process.env.DB_USERNAME), process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "postgres"
 });
